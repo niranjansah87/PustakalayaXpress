3eb46cd110409240ab3e4f73cbf2d9e11d1ca6a2
niranjan87 <2100090187csit@gmail.com>
2025-06-25 15:02:59 +0530

    Update user book list URL pattern
    Changed the URL for listing books by user from 'books/<int:user_id>/' to 'books/users/<int:user_id>/' to avoid conflict with book detail route and improve URL clarity.

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -4,7 +4,7 @@ from .views import BookListCreate, BookUpdate, BookDelete, BookListByUser,BookDe
 urlpatterns = [
     path('books/', BookListCreate.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
-    path('books/<int:user_id>/', BookListByUser.as_view(), name='book-list-by-user'),
+    path('books/users/<int:user_id>/', BookListByUser.as_view(), name='book-list-by-user'),
     path('books/<int:pk>/update/', BookUpdate.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDelete.as_view(), name='book-delete'),
 ]
